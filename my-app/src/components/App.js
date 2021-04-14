e3af9306bd66e570eabe7a17b0d691f62622ecb4
camilla-bachna <camilla.bachna@gmail.com>
2021-04-14 17:54:42 +0200

    add heading, form and footer and basic styles and favicon

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -1,12 +1,57 @@
-import '../stylesheets/App.css';
+/* import { Route, Switch } from 'react-router-dom'; */
+import '../stylesheets/App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello world</h1>
+    <>
+      <header className="header">
+        <h1 className="header__heading">Welcome to the Padel paradise</h1>
       </header>
-    </div>
+      <main className="main">
+        <form className="main__form">
+          <label htmlFor="user" className="main__form--label">
+            User
+          </label>
+          <input
+            type="text"
+            id="user"
+            className="main__form--input"
+            placeholder="add username"
+          />
+          <label htmlFor="match" className="main__form--label">
+            Pin
+          </label>
+          <input
+            type="text"
+            id="match"
+            className="main__form--input"
+            placeholder="add the game pin"
+          />
+          <label htmlFor="gender" className="main__form--label">
+            Gender
+          </label>
+          <select
+            name="gender"
+            id="gender"
+            value="gender"
+            className="main__form--select"
+          >
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+          </select>
+          <button type="submit" className="main__form--button">
+            Go
+          </button>
+        </form>
+      </main>
+      <footer className="footer">
+        <small className="footer__note">
+          This app is designed to help you find the right court. Just choose
+          your username, add the pin of the match and choose the gender. That's
+          it :)
+        </small>
+      </footer>
+    </>
   );
 }
 
